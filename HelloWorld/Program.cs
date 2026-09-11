41bb29e1deba7ac4ad4e072b7a8d071b3db1951b
chipollina <poli345678@mail.ru>
2026-09-11 19:12:14 +0300

    Step 7: variables and data types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,27 @@
-﻿Console.WriteLine("Привет от Исп-241!");
-Console.WriteLine("Автор:Домрачева Полина");
-Console.WriteLine("Год: 2026");
+﻿// int age = 20;
+// double gpa = 4.5;
+// bool isStudent = true;
+// string name = "Иван";
+
+// Console.WriteLine(name);
+// Console.WriteLine(age);
+// Console.WriteLine(gpa);
+// Console.WriteLine(isStudent);
+string firstName = "Полина";
+string lastName = "Домрачева";
+string group = "Исп-241";
+int birthdayYear = 2008;
+double gpa = 4;
+bool hasScholarship = true;
+int currentYear = 2026;
+int age = currentYear - birthdayYear;
+Console.WriteLine("Студенческое удостоверение");
+Console.WriteLine($"Имя:{firstName} {lastName}");
+Console.WriteLine($"группа:{group}");
+Console.WriteLine($"возраст:{age} лет");
+Console.WriteLine($"средний балл:{gpa} ");
+Console.WriteLine($"стипендия:{hasScholarship}");
+Console.Write("\nВведите ваш любимый предмет: ");
+string subject = Console.ReadLine();
+Console.WriteLine($"Отлично! {firstName} любит {subject}. ");
+
